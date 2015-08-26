b01a70dc0d3c184a6a6c6b889535cc9f013f0118
Koshal Thirumalai <kosh@koshal.org>
2015-08-26 13:33:06 -0700

    new mcts endpoint address

---
+++ mcts/v2/src/org/nms/mcts/MctsClient.java
@@ -0,0 +1,47 @@
+package org.nms.mcts;
+
+import org.apache.axis.message.MessageElement;
+
+import javax.xml.namespace.QName;
+import javax.xml.rpc.Call;
+import javax.xml.rpc.ServiceException;
+import javax.xml.rpc.encoding.TypeMappingRegistry;
+import javax.xml.rpc.handler.HandlerRegistry;
+import java.net.URL;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Iterator;
+
+/**
+ * MCTS client to sync data
+ */
+public class MctsClient {
+
+    public static void main(String[] args) {
+
+        try {
+            MctsServiceLocator serviceLocator = new MctsServiceLocator();
+            IMctsService dataService = serviceLocator.getbasicEndpoint();
+
+            System.out.println("Hello World");
+            System.out.println(serviceLocator.getbasicEndpointAddress());
+            DS_GetAnmAshaDataResponseDS_GetAnmAshaDataResult result =
+                    dataService.DS_GetAnmAshaData("mcts-Orissa", "OMyhYekc5H2f+0dAOiHVKBh5m4FTdys68LKG0+dqK/c=", "20-08-2015", "24-08-2015", "21");
+            DS_GetChildDataResponseDS_GetChildDataResult childDataResult = dataService.DS_GetChildData("mcts-Orissa", "OMyhYekc5H2f+0dAOiHVKBh5m4FTdys68LKG0+dqK/c=", "20-08-2015", "24-08-2015", "21");
+            for (MessageElement me : result.get_any()) {
+                System.out.println(me.toString());
+            }
+
+            for (MessageElement me : childDataResult.get_any()) {
+                System.out.println(me.toString());
+            }
+
+        } catch (RemoteException re) {
+            System.out.println(re.toString());
+        } catch (ServiceException se) {
+            System.out.println(se.toString());
+        }
+
+
+    }
+}
---
+++ mcts/v2/src/org/nms/mcts/MctsService.java
@@ -0,0 +1,16 @@
+/**
+ * MctsService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package org.nms.mcts;
+
+public interface MctsService extends javax.xml.rpc.Service {
+    public java.lang.String getbasicEndpointAddress();
+
+    public org.nms.mcts.IMctsService getbasicEndpoint() throws javax.xml.rpc.ServiceException;
+
+    public org.nms.mcts.IMctsService getbasicEndpoint(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
